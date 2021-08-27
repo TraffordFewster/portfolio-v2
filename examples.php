a9a5bde184cb99cd258c19d7a083cdfe1daf249b
Trafford Fewster <trafford.fewster@netmatters-scs.com>
2021-08-27 11:40:05 +0100

    PHPify + Contact Form
    Kinda forgot to make commits while making it and did it all in one

---
+++ examples.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "includes/head.php"; ?>
+
+    <link rel="stylesheet" href="dist/prism.css">
+    <script src="dist/prism.js" defer></script>
+    <script src="dist/exampleFiller.js" defer></script>
+</head>
+<body>
+
+    <?php include "includes/sideMenu.php"; ?>
+
+    <div id="projectModalBackground" style="display: none;">
+        <div id="projectModal">
+            <pre><code id="PMCode" class="language-lua">print('hello world')</code></pre>
+            <div class="addPadding">
+                <h4 id="PMTitle">Placeholder</h4>
+                <p id="PMDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac sagittis nulla. Etiam interdum leo rhoncus nunc aliquet, quis commodo neque congue. Integer ultricies velit dolor, eget viverra dui tincidunt id. Nulla facilisi. Nullam posuere ligula in dolor porta egestas. Vivamus ac lacinia magna. Etiam congue leo ut quam iaculis mollis. Phasellus risus dolor, maximus sed dolor sit amet, porta aliquam lectus. Aliquam erat volutpat. Nullam faucibus diam vel rhoncus consectetur. Proin a lacus elementum, faucibus tortor eget, mollis leo.</p>
+                <div id="flexHack">
+                    <ul id="PMLanguages">
+                        <li><i class="fab fa-html5"></i></li>
+                        <li><i class="fab fa-css3-alt"></i></li>
+                        <li><i class="fab fa-sass"></i></li>
+                        <li><i class="fab fa-php"></i></li>
+                    </ul>
+
+                    <ul id="links">
+                        <li><a id="PMGit" href="#" class="coolHover"><i class="fab fa-github-square"></i></a></li>
+                        <li><a id="PMLink" href="#" class="coolHover"><i class="fas fa-link"></i></a></li>
+                    </ul>
+                </div>
+        
+            </div>
+        </div>
+    </div>
+
+    <main>
+        <header>
+            <div id="titleHolder">
+                <h1 class="animateTyping">Code Examples</h1>
+                <h3></h3>
+            </div>
+        </header>
+
+        <div id="examples">
+            <article id="e0">
+                <pre><code class="language-lua">print('hello world')</code></pre>
+                <h3>Basic Hello World</h3>
+                <button>Details <i class="fas fa-arrow-right" aria-hidden="true"></i></button>
+            </article>
+            <article id="e1"></article>
+            <article id="e2"></article>
+            <article id="e3"></article>
+            <article id="e4"></article>
+            <article id="e5"></article>
+        </div>
+    </main>
+</body>
---
+++ includes/sideMenu.php
@@ -0,0 +1,26 @@
+<nav id="sideMenu" class="">
+    <div id="initials"><a href="/" class="titleFont">T</a></div>
+    <hr>
+    <ul>
+        <li><a href="about.php" class="coolHover">About Me</a></li>
+        <li><a href="index.php#projects" class="coolHover">My Portfolio</a></li>
+        <li><a href="examples.php" class="coolHover">Coding Examples</a></li>
+        <li><a href="scs.php" class="coolHover">SCS Scheme</a></li>
+    </ul>
+    <hr>
+    <div style="margin: auto; flex: 1;"></div>
+    <hr>
+    <ul>
+        <li><a href="index.html#contactForm" class="coolHover">Contact Me</a></li>
+    </ul>
+    <hr>
+    <ul id="socialMediaList">
+        <li><a target="_blank" href="https://github.com/TraffordFewster"><i class="fab fa-github-square"></i></a></li>
+        <li><a target="_blank" href="https://www.linkedin.com/in/trafford-fewster/"><i class="fab fa-linkedin"></i></a></li>
+    </ul>
+    <button id="sideMenuToggle">
+        <span class="titleFont stage1">T</span>
+        <span class="titleFont stage2">&#60;&#60;</span>
+        <span class="titleFont stage3">&#62;&#62;</span>
+    </button>
+</nav>
